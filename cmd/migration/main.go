e0dafc2067ce96f68ea2ca58498f1c3285d19274
vasarostik <ros666666@gmail.com>
2019-08-16 13:35:39 +0300

    docker db migration fixed

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -1,48 +1,62 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	go_blog "github.com/vasarostik/go_blog/pkg/utl/model"
-	"log"
-	"strings"
-	"github.com/vasarostik/go_blog/pkg/utl/secure"
 	"github.com/go-pg/pg"
 	"github.com/go-pg/pg/orm"
+	"github.com/vasarostik/go_blog/pkg/utl/config"
+	go_blog "github.com/vasarostik/go_blog/pkg/utl/model"
+	"github.com/vasarostik/go_blog/pkg/utl/secure"
+	"log"
+	"strings"
 )
 
 func main() {
-	dbInsert := `
-	INSERT INTO public.roles VALUES (100, 100, 'SUPER_ADMIN');
-	INSERT INTO public.roles VALUES (110, 110, 'ADMIN');
-	INSERT INTO public.roles VALUES (120, 120, 'COMPANY_ADMIN');
-	INSERT INTO public.roles VALUES (130, 130, 'LOCATION_ADMIN');
-	INSERT INTO public.roles VALUES (200, 200, 'USER');`
-	queries := strings.Split(dbInsert, ";")
 
+	cfgPath := flag.String("p", "./cmd/api/conf.local.yaml", "Path to config file")
+	flag.Parse()
 
-	db := pg.Connect(&pg.Options{
-		User: "postgres",
-		Password: "example",
-		Database: "postgres",
-		Addr: "db:5432",
-	})
+	cfg, err := config.Load(*cfgPath)
 
-
-
-	_, err := db.Exec("SELECT 1")
 	checkErr(err)
-	createSchema(db, &go_blog.Role{}, &go_blog.User{}, &go_blog.Post{})
 
-	for _, v := range queries[0 : len(queries)-1] {
-		_, err := db.Exec(v)
-		checkErr(err)
+	u, err := pg.ParseURL(cfg.DB.PSN)
+
+	if err != nil {
+		println("Can`t parse connection string!")
 	}
 
-	sec := secure.New(1, nil)
+	db := pg.Connect(u)
 
-	userInsert := `INSERT INTO public.users (id, created_at, updated_at, first_name, last_name, username, password, active, role_id) VALUES (1, now(),now(),'Admin', 'Admin', 'admin', '%s', true, 100);`
-	_, err = db.Exec(fmt.Sprintf(userInsert, sec.Hash("admin")))
+	_, err = db.Exec("SELECT 1")
 	checkErr(err)
+
+	exists,err:= db.Model(&go_blog.Role{}).Exists()
+	exists,err = db.Model(&go_blog.User{}).Exists()
+	exists,err = db.Model(&go_blog.Post{}).Exists()
+
+	if (exists == false) {
+
+		dbInsert := `
+			INSERT INTO public.roles VALUES (100, 100, 'SUPER_ADMIN');
+			INSERT INTO public.roles VALUES (110, 110, 'ADMIN');
+			INSERT INTO public.roles VALUES (200, 200, 'USER');`
+		queries := strings.Split(dbInsert, ";")
+
+		createSchema(db, &go_blog.Role{}, &go_blog.User{}, &go_blog.Post{})
+
+		for _, v := range queries[0 : len(queries)-1] {
+			_, err := db.Exec(v)
+			checkErr(err)
+		}
+
+		sec := secure.New(1, nil)
+
+		userInsert := `INSERT INTO public.users (id, created_at, updated_at, first_name, last_name, username, password, active, role_id) VALUES (1, now(),now(),'Admin', 'Admin', 'admin', '%s', true, 100);`
+		_, err = db.Exec(fmt.Sprintf(userInsert, sec.Hash("admin")))
+		checkErr(err)
+	}
 }
 
 func checkErr(err error) {
